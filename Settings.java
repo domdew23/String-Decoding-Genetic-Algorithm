2a2e8f04e12399678034272d8efa75f2f6b15348
domdew23 <ddewhurs@oswego.edu>
2017-12-29 14:32:53 -0500

    refractoring

--- Settings.java
+++ Settings.java
@@ -2,7 +2,7 @@ public class Settings {
 	
 	public static final String CHARS = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()-_+=[]{}<>,.?/:;'\"\\";
 	public static final int POP_SIZE = 400;
-	public static final String GOAL = "The quick brown fox jumps over the lazy dog.";
+	public static final String GOAL = "The quick brown fox jumps over the lazy dog. I am also a great teen boy with only the best of intentions so never forget that!!!";
 	public static final int GOAL_SIZE = GOAL.length();
 	public static final double MUTATION_RATE = 0.0057;
 	public static final int THRESHOLD = POP_SIZE/200;
